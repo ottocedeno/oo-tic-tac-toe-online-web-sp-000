f247058e594609c09b70ec4c4877feb38cc3b313
Otto Cedeno <otto@ottostacos.com>
2019-12-05 17:30:28 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -60,6 +60,8 @@ class TicTacToe
 
   def won?
     WIN_COMBINATIONS.each do |win_combo|
+      binding.pry
+
       if win_combo.all? {|win_index| position_taken?(win_index) == true }
         return win_combo
       end
