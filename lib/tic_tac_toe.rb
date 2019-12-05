dac35d09473409cd61e78f5b9881a1f20fa3b76a
Otto Cedeno <otto@ottostacos.com>
2019-12-05 17:52:27 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -75,9 +75,9 @@ class TicTacToe
 
 end
 
-game1 = TicTacToe.new
-# game1.move(1, "X")
-# game1.move(0, "X")
-# game1.move(2, "X")
-
-game1.full?
+# game1 = TicTacToe.new
+# # game1.move(1, "X")
+# # game1.move(0, "X")
+# # game1.move(2, "X")
+#
+# game1.full?
